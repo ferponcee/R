50cb3e8463be044bdd085fff00ac744dbd6dbcca
Manuel Villalpando Linares <A01352033@tec.mx>
2024-01-12 12:34:49 -0600

    Indentacion y orden del codigo

--- Tarea colaborativa 1.R
+++ Tarea colaborativa 1.R
@@ -1,19 +1,25 @@
 # Ejercicio 1
 x <- c(10, 72, 18, -11, 6, -34, 291)
-cat("Ejercicio 1:\n", "Vector x:", x, "\n\n")
+cat("Ejercicio 1:\n", 
+    "Vector x:", x, "\n\n")
 
 # Ejercicio 2
 media_x <- mean(x)
 desviacion_estandar_x <- sd(x)
 varianza_x <- var(x)
 est.x <- c(media_x, desviacion_estandar_x, varianza_x)
-cat("Ejercicio 2:\n", "Media:", est.x[1], "\nDesviación Estándar:", est.x[2], "\nVarianza:", est.x[3], "\n\n")
+cat("Ejercicio 2:\n", 
+    "Media:", est.x[1], 
+    "\nDesviación Estándar:", est.x[2], 
+    "\nVarianza:", est.x[3], "\n\n")
 
 # Ejercicio 3
 secuencia <- seq(10, 80)
 media_25_95 <- mean(secuencia[secuencia >= 25 & secuencia <= 95])
 suma_51_93 <- sum(secuencia[secuencia >= 51 & secuencia <= 93])
-cat("Ejercicio 3:\n", "Media de números del 25 al 95:", media_25_95, "\nSuma de números del 51 al 93:", suma_51_93, "\n\n")
+cat("Ejercicio 3:\n", 
+    "Media de números del 25 al 95:", media_25_95, 
+    "\nSuma de números del 51 al 93:", suma_51_93, "\n\n")
 
 # Ejercicio 4
 vector_aleatorio <- sample(-100:500, 10, replace = TRUE)
@@ -24,7 +30,8 @@ vector_fibonacci <- c(1, 1)
 for (i in 3:10) {
   vector_fibonacci[i] <- vector_fibonacci[i - 1] + vector_fibonacci[i - 2]
 }
-cat("Ejercicio 5:\n", "Vector Fibonacci:", vector_fibonacci, "\n\n")
+cat("Ejercicio 5:\n", 
+    "Vector Fibonacci:", vector_fibonacci, "\n\n")
 
 # Ejercicio 6
 vector_prueba <- c(1, -20, -3, 40, -5, 60)
@@ -69,7 +76,8 @@ multiplica <-function(x, y) {
   return(x * y)
 }
 result7 = multiplica(c(20, 10), c(3,4))
-cat("Ejercicio 7:\n", "Multiplicación de vectores:", result7, "\n\n")
+cat("Ejercicio 7:\n", 
+    "Multiplicación de vectores:", result7, "\n\n")
 
 #Ejercicio 8 
 cuenta <- function(vector, valor) {
