fec42f93cebb7049f66ca056bb64a16785f6cd81
Manuel Villalpando Linares <A01352033@tec.mx>
2024-01-11 18:18:01 -0600

    ejercicios 1-7 colab 1

--- Tarea colaborativa 1.R
+++ Tarea colaborativa 1.R
@@ -19,4 +19,40 @@ cat("Ejercicio 3:\n", "Media de números del 25 al 95:", media_25_95, "\nSuma de
 vector_aleatorio <- sample(-100:500, 10, replace = TRUE)
 cat("Ejercicio 4:\n", "Vector aleatorio:", vector_aleatorio, "\n")
 
+# Ejercicio 5 
+vector_fibonacci <- c(1, 1)
+for (i in 3:10) {
+  vector_fibonacci[i] <- vector_fibonacci[i - 1] + vector_fibonacci[i - 2]
+}
+cat("Ejercicio 5:\n", "Vector Fibonacci:", vector_fibonacci, "\n\n")
 
+# Ejercicio 6
+vector_prueba <- c(1, -20, -3, 40, -5, 60)
+vector_prueba2 <- c(10, -20, 30, -40, 50, -60)
+
+# Función maximo
+maximo <- function(vector) {
+  max <- vector[1]
+  for (i in 2:length(vector)) {
+    if (vector[i] > max) {
+      max <- vector[i]
+    }
+  }
+  return(max)
+}
+
+# Función minimo
+minimo <- function(vector) {
+  min <- vector[1]
+  for (i in 2:length(vector)) {
+    if (vector[i] < min) {
+      min <- vector[i]
+    }
+  }
+  return(min)
+}
+
+# Ejercicio 7
+function multiplica(x, y) {
+  return(x * y)
+}
